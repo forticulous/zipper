3d6f6d341c5afdef3cc144e726e25cf634f0f696
Fort <forticulous@gmail.com>
2016-02-04 18:48:28 -0800

    formatting

--- examples/main.rs
+++ examples/main.rs
@@ -6,11 +6,11 @@ use std::path::Path;
 use zipper::Archive;
 
 fn main() {
-  let filename = env::args().nth(1).expect("Missing filename argument");
-  let path: &Path = Path::new(&filename);
+    let filename = env::args().nth(1).expect("Missing filename argument");
+    let path: &Path = Path::new(&filename);
 
-  let mut archive = Archive::new(path).expect("Failed to open archive");
-  //archive.print_info().expect("print_info panic");
-  archive.unzip().expect("unzip panic");
+    let mut archive = Archive::new(path).expect("Failed to open archive");
+    //archive.print_info().expect("print_info panic");
+    archive.unzip().expect("unzip panic");
 }
 
